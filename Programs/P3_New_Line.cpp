9161eb2535fad9395a3a3b479da2e912466251c8
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-07 15:34:51 +0530

    Create P3_New_Line.cpp
    Added program 3 (new line)

---
+++ Programs/P3_New_Line.cpp
@@ -0,0 +1,10 @@
+/* This program will print text in new line using cout and "\n" */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+  return 0;
+}
