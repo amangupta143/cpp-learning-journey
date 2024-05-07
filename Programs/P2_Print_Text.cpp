9a1fd36f2601a4a6756ae36b49a31ee051b6557b
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-07 15:31:14 +0530

    Create P2_Print_Text.cpp
    Added 2nd program

---
+++ Programs/P2_Print_Text.cpp
@@ -0,0 +1,10 @@
+/* This program will print text using cout */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  cout << "I am learning C++";
+  return 0;
+}
