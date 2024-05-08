c6e5547835f71e17a6fb15f12fb31e85fa04ebcd
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-08 21:30:34 +0530

    Comparison Operators
    Added program 11: Comparison Operators

---
+++ Programs/P11_Comparison_Operators.cpp
@@ -0,0 +1,23 @@
+/* 
+ Comparison operators are used to compare two values (or variables).
+ This is important in programming, because it helps us to find answers and make decisions.
+
+ The return value of a comparison is either 1 or 0, which means true (1) or false (0).
+
+ ==	  Equal to
+ !=	  Not equal
+ >	  Greater than
+ <	  Less than
+ >=	  Greater than or equal to
+ <=	  Less than or equal to
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 9;
+  int y = 4;
+  cout << (x > y); // returns 1 (true) because 9 is greater than 4
+  return 0;
+}
