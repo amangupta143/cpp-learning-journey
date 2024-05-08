7d0365c551c1daff402ac693c7f6b4248f667874
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-08 10:31:23 +0530

    Create P4_Comment.cpp
    Added P4 singled-lined and multi-lined comments

---
+++ Programs/P4_Comment.cpp
@@ -0,0 +1,20 @@
+/* 
+
+Comments can be used to explain C++ code, and to make it more readable. 
+It can also be used to prevent execution when testing alternative code. 
+Comments can be singled-lined or multi-lined. 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // This is a single-lined comment.
+  cout << "I am learning C++.";
+  /*
+  This is a multi-lined comment.
+  Multi-lined comments are usually used for long comments.
+  */
+  return 0;
+}
