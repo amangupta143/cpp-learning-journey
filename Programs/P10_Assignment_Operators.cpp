6985f2d1f5c89e5311220cba649af38be51c2832
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-08 21:26:58 +0530

    Assignment Operators
    Added program 10: Assignment Operators

---
+++ Programs/P10_Assignment_Operators.cpp
@@ -0,0 +1,14 @@
+/* 
+ Assignment operators are used to assign values to variables.
+ Example: "=", "+=", "*=", "^=", etc.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  x += 5;
+  cout << x;
+  return 0;
+}
