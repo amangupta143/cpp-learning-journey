b22cbeb9d62dba459eff5f7bf598770b37a3c501
amangupta143 <109453339+amangupta143@users.noreply.github.com>
2024-05-08 11:47:38 +0530

    User Input
    Added Program 7: User Input

---
+++ Programs/P7_User_Input.cpp
@@ -0,0 +1,14 @@
+/* 
+ cin is a predefined variable that reads data from the keyboard with the extraction operator (>>).
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x;
+  return 0;
+}
